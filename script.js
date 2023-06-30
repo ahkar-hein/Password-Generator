29aa2645fe7c283961bb59cd5f92fc6917f9ee85
Ahkar Hein <ahkarhein04@gmail.com>
2023-06-29 23:00:39 -0700

    create object for characters

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Create Object for password criteria
+var Characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numeric: "0123456789",
+  special: "!@#$%^&*()_+-=[]{};':\"\\|,.<>/?"
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
